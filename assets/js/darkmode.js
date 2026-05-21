0ee3699f30e5dc063baf05d87f4a4e7c279b901a
DeiviDiaz <deividiazg5@gmail.com>
2026-05-21 16:59:43 -0500

    implementación de modo oscuro con localStorage

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,1 +1,30 @@
-// Modo oscuro
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+const body = document.body;
+
+// Verificar si existe tema guardado
+const darkMode = localStorage.getItem("darkMode");
+
+if (darkMode === "active") {
+
+    body.classList.add("dark-mode");
+
+}
+
+// Evento botón
+darkModeBtn.addEventListener("click", () => {
+
+    body.classList.toggle("dark-mode");
+
+    // Guardar estado
+    if (body.classList.contains("dark-mode")) {
+
+        localStorage.setItem("darkMode", "active");
+
+    } else {
+
+        localStorage.setItem("darkMode", null);
+
+    }
+
+});
